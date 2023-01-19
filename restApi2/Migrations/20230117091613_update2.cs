8be8b54c7a3c62071f990b3fa398edd4697e48ac
Balaji A M <219696@ust-global.com>
2023-01-19 15:28:18 +0530

    first commit

---
+++ restApi2/Migrations/20230117091613_update2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace restApi2.Migrations
+{
+    /// <inheritdoc />
+    public partial class update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrderNo",
+                table: "order",
+                newName: "ItemId");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerID",
+                table: "order",
+                newName: "quantity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "tableNumber",
+                table: "items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tableNumber",
+                table: "items");
+
+            migrationBuilder.RenameColumn(
+                name: "quantity",
+                table: "order",
+                newName: "CustomerID");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemId",
+                table: "order",
+                newName: "OrderNo");
+        }
+    }
+}
---
+++ restApi2/model/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace restApi2.model
+{
+    public class Order
+    {
+        [KeyAttribute()]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long OrderID { get; set; }
+        public decimal GTotal { get; set; }
+        public string ItemId { get; set; }
+        public int quantity { get; set; }
+
+    //    public ICollection<Item> items { get; set; }
+
+    }
+}
